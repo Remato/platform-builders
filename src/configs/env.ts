d15afc0168dd17c14c64068bc263f8af00af09a1
Remato <renatobcostaa@gmail.com>
2021-03-08 02:26:23 -0300

    add api keys to debug application

---
+++ src/configs/env.ts
@@ -0,0 +1,9 @@
+// Como é uma aplicação de testes estou deixando o arquivo
+// se o .env para poder ser testado livremente
+
+const keys = {
+  weatherKey: '7c14d86b5a706498d3cc34929ac5fbd8',
+  reverseGeolocationKey: 'pk.18f674a26ab05b315ac3d8d27caafdd8',
+};
+
+export default keys;
